674fc69e474cf0d5ce8192348a9c913ac9bd0222
KiwiNipCheese <juliussauchelli@gmail.com>
2021-11-24 09:11:06 -0800

    Update computercraft.lua

--- computercraft.lua
+++ computercraft.lua
@@ -1,16 +1,21 @@
 bReader =  peripheral.wrap("left");
-function getContents(chestData) 
-    if chestData["Items"][0] == nil then
+function getContents(chestData,invTag1,invTag2) 
+    if not invTag2 then
+    invData = chestData[invTag1][invTag2]
+    else
+    invData = chestData[invTag1]
+    end
+    if invData[0] == nil then
         return nil
     end
-    local itemAmt = table.getn(chestData["Items"])
+    local itemAmt = table.getn(invData)
     local tempDict = {}
     for i=0,itemAmt do 
-        local tString = chestData.Items[i].id
+        local tString = invData[i].id
         if(tempDict[tString]) then
-            tempDict[tString] = tempDict[tString] + chestData["Items"][i]["Count"]
+            tempDict[tString] = tempDict[tString] + invData[i]["Count"]
         else
-            tempDict[tString] = chestData["Items"][i]["Count"]
+            tempDict[tString] = invData[i]["Count"]
         end
     end
     return tempDict
