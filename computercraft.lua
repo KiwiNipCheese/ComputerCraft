ae4a9efc3877e1e83f8a522bd158b754da05f7e9
KiwiNipCheese <juliussauchelli@gmail.com>
2021-11-26 07:23:59 -0800

    Update computercraft.lua

--- computercraft.lua
+++ computercraft.lua
@@ -1,28 +1,90 @@
-bReader =  peripheral.wrap("left");
-function getContents(chestData,invTag1,invTag2) 
-    if not invTag2 then
-    invData = chestData[invTag1][invTag2]
-    else
-    invData = chestData[invTag1]
-    end
-    if invData[0] == nil then
-        return nil
-    end
-    local itemAmt = table.getn(invData)
-    local tempDict = {}
-    for i=0,itemAmt do 
-        local tString = invData[i].id
+iChest =  peripheral.wrap("top");
+fCrafter = peripheral.wrap("left");
+oChest = peripheral.wrap("bottom");;
+oChestSide = "bottom";
+fCrafterSide = "left";
+recipes = 
+{
+    {"draconicevolution:wyvern_crafting_injector"  
+        ,
+        {
+            {"draconicevolution:basic_crafting_injector",1}
+            {"minecraft:diamond",4}
+            {"draconicevolution:draconium_block",1}
+            {"draconicevolution:draonium_core",2}
+        }
+    }
+}
+function getContents(inventory)
+    local invData = inventory.list();
+    local tempDict = {};
+    for i in next,invData do 
+        local tString = invData[i].name;
         if(tempDict[tString]) then
-            tempDict[tString] = tempDict[tString] + invData[i]["Count"]
+            table.insert(tempDict,i,{invData[i]["name"],tempDict[tString] + invData[i]["count"]});
         else
-            tempDict[tString] = invData[i]["Count"]
+            tempDict[i] = invData[i]["count"];
         end
     end
-    return tempDict
+    return tempDict;
 end
-local test = getContents(bReader.getBlockData())
-print(table.getn(test))
-for i in ipairs(test) do
-    print(test[i])
+function checkRecipe(recipeList,contents)
+    local recipeBool = false;
+    for recipe in next,recipeList do
+        for ingredients in next,recipe[2] do
+            if recipeBool == false && recipe[2][1][1] == ingredients[1] then
+                recipeBool = true;
+            end
+            for chestContents in next,contents do
+                if chestContents[1] == ingredients[1] then
+                    if chestContents[2] % ingredients[2] == 0 then
+                    else
+                        recipeBool = false;
+                        break 
+                    end
+                end
+            end
+        end
+        if recipeBool then
+            return recipe;
+        end
+    end
+    return false
 end
-print(test);
+function rpushToChest(itemID,count,chest,side)
+    local chestList = chest.list();
+    local currentCount = count;
+    local j = 1;
+    for i,k in next,chestList do
+        if k.name == itemID then
+            if ( k.count >= currentCount) then
+                currentCount = currentCount - chest.pushItems(side,j,currentCount);
+                return true;
+            else
+                currentCount = currentCount - chest.pushItems(side,j,currentCount);
+            end
+            
+        end
+        j = j+1;
+    end
+    return false
+end
+while true do
+    local recipeCheck = checkRecipe(recipes,getContents(iChest))
+    if recipeCheck then
+        rpushToChest(recipeCheck[2][1][1],recipeCheck[2][1][2],iChest,fCrafter,1);
+        for i=2,table.getn(recipeCheck) do
+            rpushToChest(recipeCheck[2][i][1],recipeCheck[2][i][2],iChest,oChest);
+        end
+        rs.setOutput("left",true);
+        os.sleep(1);
+        rs.setOuptut("left",false);
+        while not (fCrafter.list[2] == nil) do
+            os.sleep(1)
+        end
+        print(recipeCheck[1],"succesfully crafted")
+        rs.setOuptut("front",true);
+        os.sleep(1);
+        rs.setOuptut("front",false);
+    end
+end
